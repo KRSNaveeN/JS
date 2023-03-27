47c31d41b2d5d4c6878c99f07908271f04bff52e
KRSNaveeN <117568376+KRSNaveeN@users.noreply.github.com>
2023-03-27 17:30:18 +0530

    Add files via upload

---
+++ dom4.js
@@ -0,0 +1,18 @@
+ document.title = "hello";
+console.dir(document);
+console.log(document.body);
+console.log(document.head);
+console.log(document.HTML);
+console.log(document.all);
+console.log(document.doctype);
+document.getElementById("header-title").textContent = "items listeners"
+let x = document.getElementById("header-title");
+x.style.backgroundColor = "blue";
+document.getElementById("main-header").style.borderBottom="solid 3px black";
+document.getElementById("krs").style.fontWeight = "bolder";
+document.getElementById("krs").style.color = "green";
+
+
+
+
+
