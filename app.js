0b5ee3ed52f9cec28f7e723311929683d15ecb9e
KRSNaveeN <117568376+KRSNaveeN@users.noreply.github.com>
2023-10-10 21:57:00 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,141 +1,159 @@
-let x = document.querySelector('#item1');
-let y = document.querySelector('#item2');
-let z = document.querySelector('#item3');
-let a = document.querySelector('#navi');
-// let m = document.querySelector('#krs');
-let ol = document.querySelector('#krs');
-a.addEventListener('click' , register);
-ol.addEventListener('click', (event)=>{
-    console.log('working');
-    if(event.target.tagName === 'BUTTON')
-    {
-        console.log('here',event);
-        const button = event.target;
-        const li = button.parentNode;
-        const ul = li.parentNode;
-        const div  = li.firstElementChild;
-        const text = div.textContent;
-        const ans = text.split("-");
-        if(button.textContent === 'delete')
-        {
-            ol.removeChild(li);
-            // axios.delete("")
-            
-            
-        }
-         if(button.textContent === 'edit')
-        {
-              x.value = ans[0];
-              y.value = ans[1];
-              z.value = ans[2];
+// axios.defaults.headers.common['X-Auth-Token'] ='sometoken'
 
+let y = document.querySelector('#addbill');
+y.addEventListener("click", x);
+let count  =1;
+let count2 = 1;
+let count3 = 1;
 
-              ul.removeChild(li);
-              
-        }
-    }
-});
-window.addEventListener('DOMContentLoaded', ()=>{
-    axios.get("https://crudcrud.com/api/11dead135d3549418caa72f228ce7483/users")
-       .then((res)=>{console.log(res.data), recall(res.data)})
-       .catch((err)=>console.log(err));
+// console.log(document.querySelector('#ram').value)
 
-})
+function x(){
+   let selectedvalue =  document.querySelector('#select').value;
+  
+   let pricevalue = document.querySelector('#price').value;
+   
+   let productvalue = document.querySelector('#product').value;
+   let newelement = document.createElement('p');
+   let b = "price is"+ pricevalue+"  - ordered"+productvalue;
+   newelement.textContent=b+" ";
 
-function recall(arr){
-    arr.map((obj)=>{
-let b = document.createElement('li');
-let div = document.createElement('div');
-// let c = x.value+"-"+y.value+"-"+z.value;
-let c = obj.name+"-"+obj.email+"-"+obj.phone;
-div.appendChild(document.createTextNode(c));
+   
+   
+   let newbtn = document.createElement('button')
+   newbtn.textContent="delete";
 
-b.appendChild(div);
-ol.appendChild(b);
-
-let d = document.createElement('button');
-let e = document.createTextNode("delete");
-d.appendChild(e);
-let m = obj._id;
-d.addEventListener('click', ()=>{
-    axios.delete(`https://crudcrud.com/api/11dead135d3549418caa72f228ce7483/users/${m}`);
-
-//    ol.removeChild(b);
-})
-d.classList="nasik";
-b.appendChild(d);
-
-
-let ab = document.createElement('button');
-let cd  = document.createTextNode('edit');
-ab.addEventListener('click', ()=>{
-    axios.delete(`https://crudcrud.com/api/11dead135d3549418caa72f228ce7483/users/${m}`);
-})
-ab.appendChild(cd);
-ab.classList = 'editb';
-b.appendChild(ab);
-
-
-    })
-}
-
-
-function register(){
-    let myobj = 
-    {
-        name : x.value,
-        email : y.value,
-        number : z.value,
-    
-    }
-let stringifiedobj = JSON.stringify(myobj);
-console.log('inside registr');
- axios.post("https://crudcrud.com/api/11dead135d3549418caa72f228ce7483/users", myobj).then((data)=>{console.log(data),console.log("inside axios")}).catch((err)=>{console.log(err)});
-// localStorage.setItem(x.value,stringifiedobj);
-let b = document.createElement('li');
-let div = document.createElement('div');
-let c = x.value+"-"+y.value+"-"+z.value;
-div.appendChild(document.createTextNode(c));
-
-b.appendChild(div);
-
-let d = document.createElement('button');
-let e = document.createTextNode("delete");
-d.appendChild(e);
-d.classList="nasik";
-b.appendChild(d);
-
-
-let ab = document.createElement('button');
-let cd  = document.createTextNode('edit');
-ab.appendChild(cd);
-ab.classList = 'editb';
-b.appendChild(ab);
-
-ol.appendChild(b);
-x.value ="";
-y.value = "";
-z.value ="";
-}
-
-
-// function deletef(e){
-    
-//     if(e.target.classList.contains('nasik'))
-// {     
-//     // console.log(1);
-//     if(confirm('are you sure?'))
-//     {
-//         // here we are dealing with button so it s parent would be li
-//        var lii = e.target.parentElement.parentElement; 
-//     //    localStorage.removeItem();
-//        ol.removeChild(lii);
+//    const config = {
+//     headers :{
+//         'Content-Type':'application/json',
+//         Authorization : 'sometoken',
+        
 //     }
-// }
-// }
+// };
 
     
+  //  axios.post("https://crudcrud.com/api/760f0d3181fb4768a613d76b32294da4/allow-cors",{price:"pricevalue", product:"productvalue", id:1});
 
+    if(selectedvalue == 'table1' && count == 1){
+       let m = document.querySelector('#t1');
+        count++;
+      
+        localStorage.setItem("VALUE1", b);
+       newbtn.setAttribute("class","table1");
 
+       m.appendChild(newelement);
+       m.appendChild(newbtn);
+       
+      
+      document.querySelector('.table1').addEventListener('click', ()=>{remove( newbtn, newelement); localStorage.removeItem('VALUE1'); count--});
+    
+      // axios.post("https://crudcrud.com/api/760f0d3181fb4768a613d76b32294da4",{price:"pricevalue", product:"productvalue", id:1});
+    }
 
+   else if(selectedvalue == 'table2' && count2 ==1){
+        let m = document.querySelector('#t2');
+        count2++;
 
+        localStorage.setItem("VALUE2", b);
+        newbtn.setAttribute("class","table2");
+
+        m.appendChild(newelement);
+        m.appendChild(newbtn);
+        
+        
+      document.querySelector('.table2').addEventListener('click', ()=>{remove( newbtn, newelement); localStorage.removeItem('VALUE2'); count2--;});
+       
+     }
+     else if(selectedvalue == 'table3' && count3 == 1){
+        let m = document.querySelector('#t3');
+        newbtn.setAttribute("class","table3");
+        count3++;
+        localStorage.setItem("VALUE3", b);
+        m.appendChild(newelement);
+        m.appendChild(newbtn);
+        
+      document.querySelector('.table3').addEventListener('click', ()=>{remove( newbtn, newelement); localStorage.removeItem('VALUE3'); count3--});
+
+     }
+
+     function remove(a1,a2){
+
+                a1.remove();
+                a2.remove(); 
+
+     }
+    }
+    console.log("running");
+
+    function reload(){
+      let x =  document.querySelector('#t1');
+      let x2 = document.querySelector('#t2');
+      let x3 = document.querySelector('#t3');
+      let z= localStorage.getItem('VALUE1');
+      let z2= localStorage.getItem('VALUE2');
+      let z3= localStorage.getItem('VALUE3');
+
+      
+      let newbtn = document.createElement('button')
+      let newbtn2 = document.createElement('button')
+      let newbtn3 = document.createElement('button')
+      let y = document.createElement('p');
+      let y2 = document.createElement('p');
+      let y3 = document.createElement('p');
+     
+      if(z != null){
+      
+      console.log(z);
+      count++;
+
+     
+      y.textContent=z;
+      x.appendChild(y);
+
+      
+      x.appendChild(newbtn);
+      newbtn.setAttribute('class', 'table1')
+      newbtn.textContent="delete";
+      document.querySelector('.table1').addEventListener('click', ()=>{y.remove(); newbtn.remove(); count--; localStorage.removeItem('VALUE1'); })
+      }
+
+      if(z2 != null){
+      
+        console.log(z2);
+        count2++;
+  
+       
+        y2.textContent=z2;
+        x2.appendChild(y2);
+  
+        
+        x2.appendChild(newbtn2);
+        newbtn2.setAttribute('class', 'table2')
+        newbtn2.textContent="delete";
+        document.querySelector('.table2').addEventListener('click', ()=>{y2.remove(); newbtn2.remove(); count2--; localStorage.removeItem('VALUE2'); })
+        }
+  
+        if(z3 != null){
+      
+            console.log(z3);
+            count3++;
+      
+           
+            y3.textContent=z3;
+            x3.appendChild(y3);
+      
+            
+            x3.appendChild(newbtn3);
+            newbtn3.setAttribute('class', 'table3')
+            newbtn3.textContent="delete";
+            document.querySelector('.table3').addEventListener('click', ()=>{y3.remove(); newbtn3.remove(); count3--; localStorage.removeItem('VALUE3'); })
+            }
+      
+    
+    }
+
+    reload();
+    //  axios.get("https://crudcrud.com/api/760f0d3181fb4768a613d76b32294da4").then((msg)=>{console.log(msg)});
+    // axios.post("https://crudcrud.com/api/760f0d3181fb4768a613d76b32294da4/allow-cors", {name:"krs", id:1}).then((ms)=>{console.log(ms)});
+    // let x5 =  axios.get("https://crudcrud.com/api/760f0d3181fb4768a613d76b32294da4/1");
+    // console.log(x5);
