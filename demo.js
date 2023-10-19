3a713875fa20fc5e58b36610afc7cfd09a2fd318
KRSNaveeN <117568376+KRSNaveeN@users.noreply.github.com>
2023-10-20 01:02:27 +0530

    Add files via upload

--- demo.js
+++ demo.js
@@ -3,19 +3,27 @@ function createPost(post){
     return new Promise((resolve, reject)=>{
         setTimeout(()=>{
             posts.push(post);
+            console.log("inside createpost");
             resolve(posts);
         },2000)
     })
 
 }
 
-function getPost(){
+async function asyncronous(post){
+  await createPost(post) ;
+ await getPost();
+ console.log("got the posts")
+
+ return posts;
+}
+function getPost(){    
     return new Promise((resolve,reject)=>{
    
         setTimeout(()=>{
             if(posts.length>=0){
                 let li='';
-        // console.log(posts);
+        console.log("inside getpost");
         posts.forEach((post)=>{
              li +=`<li>${post.title}</li>`;
             //  console.log(li);
@@ -30,7 +38,13 @@ function getPost(){
         },1000)
     })
 }
-createPost({title:"ravan"}).then(()=>{getPost()});
+
+// asyncronous({title:"laksmi"}).then((x)=>{console.log(x)});
+
+// async functions returns aa promise and you can perform .then on it and you can also use try catch block on it for error catching
+
+// 
+// createPost({title:"ravan"}).then(()=>{getPost()});
 
 
 // let p1 = new Promise((resolve, reject)=>{
@@ -55,8 +69,17 @@ function updateLastUserActivityTime(){
     })
 }
 
+
+ async function delete1(){
+    await deletePost();
+    console.log("post deleted")
+    return 0;
+
+};
+
 function deletePost(){
     return new Promise((resolve, reject)=>{
+
         posts.pop();
         if(posts.length>0)
         {
@@ -66,7 +89,46 @@ function deletePost(){
             reject("no posts")
         }
        
+       
     })
 }
 
-Promise.all([createPost({title:"laxmi"}), updateLastUserActivityTime()]).then((values)=>{console.log(values); return deletePost()}).then((ans)=>{console.log(ans)});
+
+
+// Promise.all([createPost({title:"laxmi"}), updateLastUserActivityTime()]).then((values)=>{console.log(values); return deletePost()}).then((ans)=>{console.log(ans)});
+
+let wifebringstickets = async ()=>{
+    
+    let x = new Promise((resolve,reject)=>{
+        
+        
+        setTimeout(()=>{console.log("wife brings tickets"); resolve()},3000);
+        
+        
+    });
+
+    let y = new Promise((resolve,reject)=>{
+        
+        
+        setTimeout(()=>{console.log("husband brings butter"); resolve()},3000);
+    })
+    function z(){
+        console.log("they go inside");
+    }
+    await x;
+    console.log("husband says lets go0");
+    console.log("but wife says hungry");
+    
+
+    await y;
+    console.log("husband brings popcorn");
+        console.log("wife needs butter too");
+    z();
+//  console.log("4")
+    
+    return "person3";
+}
+console.log("person1");
+console.log("person2")
+wifebringstickets().then((ans)=>{console.log(ans)});
+console.log("person4")
